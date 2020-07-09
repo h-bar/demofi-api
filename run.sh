eafd5762116638f91de41fe9f2afc75ec81c528f
Yan Zhang <663yan@gmail.com>
2020-07-09 14:20:25 -0400

    change demo.py to main.py

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #! /usr/bin/bash
 
 if [ $1 == "dev" ]; then
-  FLASK_ENV=development python3 ./demo.py
+  FLASK_ENV=development python3 ./main.py
 else
-  gunicorn -w 4 -b 0.0.0.0:5000 demo:app
+  gunicorn -w 4 -b 0.0.0.0:5000 main:app
 fi
