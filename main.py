14a820a04d2bc1c3215b5f3ed02c38444cf336e4
Yan Zhang <663yan@gmail.com>
2020-07-10 22:38:56 -0400

    move db into main

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from demofi.app import demo_app
 from my_demo import run_model, model_db
+from demofi import db
 
+model_db = db.sqliteDB('./db.db', './schema.sql')
 app = demo_app(run_model, model_db).app
 
 if __name__ == "__main__":
--- my_demo.py
+++ my_demo.py
@@ -1,6 +1,2 @@
-from demofi import db
-
-def run_model(data: str) -> {}:
-  return hash(data)
-
-model_db = db.sqliteDB('./db.db', './schema.sql')
+def run_model(data: {}) -> {}:
+  return hash(str(data))
