fe5ad6e1bce9c59f6bcd5f9f0d2096d5a21fb5b9
Yan Zhang <663yan@gmail.com>
2020-07-16 23:56:26 -0400

    use a tagger as model

--- my_demo.py
+++ my_demo.py
@@ -1,11 +1,20 @@
 import json
 from demofi.model import abstractModel
+
+import nltk
 class my_model(abstractModel):
   def init_model(self):
+    nltk.download('punkt')
+    nltk.download('averaged_perceptron_tagger')
     pass
 
   def run_model(self, data: {}, param: {}) -> {}:
-    return {'str': json.dumps(data)}
+    text = nltk.word_tokenize(data['content'])
+    tagged = nltk.pos_tag(text)
+    result = {}
+    for t in tagged:
+      result[t[0]] = t[1]
+    return result
 
   def destroy_model(self):
     pass
